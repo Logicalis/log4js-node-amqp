9af4ebda3822a468ef01c3f92a03256cd38ef90f
João Henrique Ferreira de Freitas <joaohf@gmail.com>
2017-01-10 16:07:09 -0200

    Log headers and deliveryInfo too

--- example/logReader/example.js
+++ example/logReader/example.js
@@ -1,3 +1,4 @@
+var util = require('util')
 var amqp = require('amqp')
 
 var config = {
@@ -24,8 +25,11 @@ var onReady = function () {
     connection.queue(config.queue.name, config.queue, function (queue) {
       queue.bind(exchange, '*')
 
-      queue.subscribe(function (message) {
-        console.log(message)
+      queue.subscribe(function (message, headers, deliveryInfo) {
+        console.log("-------------------")
+        console.log(util.format("deliveryInfo: %j", deliveryInfo))
+        console.log(util.format("headers: %j", headers))
+        console.log(util.format("message: %j", message))
       })
     })
   })
